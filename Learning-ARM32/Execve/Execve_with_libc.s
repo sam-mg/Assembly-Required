7d92bce8ae62c84ba9ba807ab583a04bf669a604
Sam MG Harish <147686318+sam-mg@users.noreply.github.com>
2024-11-25 23:55:36 +0530

    Add Execve

---
+++ Learning-ARM32/Execve/Execve_with_libc.s
@@ -0,0 +1,12 @@
+.text
+
+.global main       // Specify the entry point of the program
+
+main:
+    ldr r0, =path  // Load the address of the binary path into r0
+
+    bl execve      // Call execve function in C
+    bl exit        // Calling exit function in C
+
+.data
+binary_path: .asciz "<the_path_of_the_executable>"
