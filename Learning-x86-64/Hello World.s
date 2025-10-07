991aea27cb3021a83578560fe761a7f24216b97d
Sam MG Harish <147686318+sam-mg@users.noreply.github.com>
2025-10-07 08:32:24 +0000

    Added Comments

--- Learning-x86-64/Hello World.s
+++ Learning-x86-64/Hello World.s
@@ -1,17 +1,17 @@
 section .data
-    msg db 'Hello, World!', 10
-    len equ $ - msg
+    msg db 'Hello, World!', 10    ; Hello World, appended with "\n"
+    len equ $ - msg               ; Length of msg
 
 section .text
-    global _main
+    global _main                  ; Entry point for the program
 
 _main:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, msg
-    mov edx, len
-    int 0x80
+    mov eax, 4                    ; Set eax as 4 (indicator for writing)
+    mov ebx, 1                    ; Set ebx as 1 (indicator for stdout)
+    mov ecx, msg                  ; Loading the address of prompt to ecx
+    mov edx, len                  ; Loading the length of prompt to edx
+    int 0x80                      ; Syscall
 
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov eax, 1                    ; Set eax as 1 (indicator for sys_exit)
+    xor ebx, ebx                  ; xor ebx with itself to make it 0
+    int 0x80                      ; Syscall
--- Learning-x86-64/Return 0.s
+++ Learning-x86-64/Return 0.s
@@ -1,7 +1,7 @@
 section .text
-    global _main
+    global _main        ; Entry point for the program
 
 _main:
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov eax, 1          ; Set eax as 1 (indicator for sys_exit)
+    xor ebx, ebx        ; xor ebx with itself to make it 0
+    int 0x80            ; Syscall
